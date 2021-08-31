55dda0397d670ccb33f1b1c0da07b2ba18fb6c8b
avocadohooman <gmolin@student.hive.fi>
2021-08-31 16:55:13 +0300

    adding graphcache and updating Me cache when login/register user

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,12 +1,43 @@
 import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react'
-import { Provider, createClient} from 'urql';
+import { Provider, createClient, dedupExchange, fetchExchange} from 'urql';
+import { cacheExchange } from '@urql/exchange-graphcache';
 import theme from '../theme';
+import { LoginMutation, MeDocument, MeQuery, RegisterMutation } from '../generated/graphql';
+
 
 const client = createClient({
   url: 'http://localhost:4000/graphql',
   fetchOptions: {
     credentials: 'include',
-  }
+  },
+  exchanges: [dedupExchange, cacheExchange({
+    updates: {
+      Mutation: {
+        login: (result: LoginMutation, args, cache, info) => {
+          cache.updateQuery({ query: MeDocument}, (data: MeQuery) => {
+            if (result.login.errors) {
+              return data;
+            } else {
+              return {
+                me: result.login.user,
+              }
+            }
+          });
+        },
+        register: (result: RegisterMutation, args, cache, info) => {
+          cache.updateQuery({ query: MeDocument}, (data: MeQuery) => {
+            if (result.register.errors) {
+              return data;
+            } else {
+              return {
+                me: result.register.user,
+              }
+            }
+          });
+        }
+      }
+    }
+  }), fetchExchange],
   
 });
 
